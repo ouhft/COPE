537dddfef2f0b9e5eb49a9388abca43e6b8d206d
Carl Marshall <carl@cm13.net>
2015-06-21 22:26:53 +0100

    Added the bootstrap3 app to help with form rendering. Basic authentication implementation started, login done. Basic (default) layout of the procurement form in place

---
+++ src/wp4/compare/forms.py
@@ -0,0 +1,49 @@
+from django.forms import ModelForm
+from .models import Donor, Organ
+
+
+class DonorForm(ModelForm):
+    class Meta:
+        model = Donor
+        fields = [
+            'retrieval_team', 'perfusion_technician', 'transplant_coordinator', 'call_received', 'retrieval_hospital',
+            'scheduled_start', 'technician_arrival', 'ice_boxes_filled', 'depart_perfusion_centre',
+            'arrival_at_donor_hospital',
+
+            'number', 'date_of_birth', 'age', 'date_of_admission', 'admitted_to_itu', 'date_admitted_to_itu',
+            'date_of_procurement', 'gender', 'weight', 'height', 'ethnicity', 'blood_group',
+
+            'diagnosis', 'diagnosis_other', 'diabetes_melitus', 'alcohol_abuse', 'cardiac_arrest',
+            'systolic_blood_pressure', 'diastolic_blood_pressure', 'diuresis_last_day', 'diuresis_last_day_unknown',
+            'diuresis_last_hour', 'diuresis_last_hour_unknown', 'dopamine', 'dobutamine', 'nor_adrenaline',
+            'vasopressine', 'other_medication_details',
+
+            'last_creatinine', 'last_creatinine_unit', 'max_creatinine', 'max_creatinine_unit',
+
+            'life_support_withdrawal', 'systolic_pressure_low', 'circulatory_arrest', 'length_of_no_touch',
+            'death_diagnosed', 'perfusion_started', 'systemic_flush_used', 'systemic_flush_used_other',
+            'heparin',
+
+            'donor_blood_1_EDTA', 'donor_blood_1_SST', 'donor_urine_1', 'donor_urine_2',
+        ]
+
+
+class OrganForm(ModelForm):
+    class Meta:
+        model = Organ
+        fields = [
+            'donor', 'location',
+
+            'removal', 'renal_arteries', 'graft_damage', 'washout_perfusion', 'transplantable',
+            'not_transplantable_reason',
+
+            'preservation',
+
+            'perfusion_possible', 'perfusion_not_possible_because', 'perfusion_started', 'patch_holder',
+            'artificial_patch_used', 'artificial_patch_size', 'artificial_patch_number', 'oxygen_bottle_full',
+            'oxygen_bottle_open', 'oxygen_bottle_changed', 'oxygen_bottle_changed_at', 'ice_container_replenished',
+            'ice_container_replenished_at', 'perfusate_measurable', 'perfusate_measure', 'perfusion_machine',
+            'perfusion_file',
+
+            'perfusate_1', 'perfusate_2',
+    ]
--- src/wp4/wp4/urls.py
+++ src/wp4/wp4/urls.py
@@ -2,10 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'wp4.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace="auth")),
     url(r'^', include('compare.urls', namespace="compare")),
 ]
