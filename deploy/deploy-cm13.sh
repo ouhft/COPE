cb5b2fea1cb5d65c575f20a375c1157728e4cd05
Carl Marshall <carl@cm13.net>
2015-09-17 22:52:04 +0100

    Updates to the deploy and update script on Webfaction

--- deploy/deploy-cm13.sh
+++ deploy/deploy-cm13.sh
@@ -11,24 +11,26 @@ PIP_RESPECT_VIRTUALENV=true
 HOME=/home/cm13
 PYTHON=python2.7
 
-
-cd $HOME/webapps/wp4_django
+echo " "
+echo "=================================================================================="
+echo " "
 date
-cd COPE
+cd $HOME/webapps/wp4_django/COPE
 PULL=$(git pull)
 echo "Repository Updated : $PULL"
 if [ "$PULL" = "Already up-to-date." ]; then
     echo No further actions are required
 else
-    cd src/wp4/
-    # TODO: Pip update after activate if requirements.txt found in git pull output
-    # TODO: pm collectstatic so that static files are copied
     source $HOME/.virtualenvs/wp4_20150514/bin/activate
+    PIP_UPDATE=$(pip install -r requirements/webfaction.txt)
+    echo $PIP_UPDATE
+    COLLECT_FILES=$(python2.7 manage.py collectstatic --noinput)
+    echo $COLLECT_FILES
     CHECK=$(python2.7 manage.py check)
     if [ "$CHECK" = "System check identified no issues (0 silenced)." ]; then
         MIGRATE=$(python2.7 manage.py migrate)
         echo "Migration completed : $MIGRATE"
-        APACHE=$(../../../apache2/bin/restart)
+        APACHE=$(../apache2/bin/restart)
         echo $APACHE
     else
         echo "System check failed : $CHECK"
