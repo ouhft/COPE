d9269aae93c18c27af784a14bc7805f264712315
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2016-01-20 16:33:16 +0000

    Added the missing migration that Fixes #22
    Also, created some tags that use the package version number to update the template display!

--- wp4/theme/templatetags/theme_tags.py
+++ wp4/theme/templatetags/theme_tags.py
@@ -1,6 +1,10 @@
 __author__ = 'carl'
 
+import time
+import os
 from django import template
+from django.conf import settings
+import wp4
 
 register = template.Library()
 
@@ -11,3 +15,20 @@ def active(request, pattern):
     if path == pattern:
         return 'active'
     return ''
+
+
+@register.simple_tag
+def version_number():
+    return wp4.__version__
+
+
+# From http://stackoverflow.com/a/23584696/3687274
+@register.simple_tag
+def version_date():
+    path = settings.ROOT_DIR + '.git'
+    return time.strftime('%b %Y', time.gmtime(os.path.getmtime(path.__str__())))
+
+
+@register.simple_tag
+def copyright_date_string():
+    return "2015-" + time.strftime('%Y', time.gmtime())
