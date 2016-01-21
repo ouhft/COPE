a4893d94c8d39954385034529d6eb562aebd2d97
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2016-01-21 14:39:20 +0000

    Offline list display pages generated. Fixes #24 Tweaks to the Nav and Breadcrumbs to bring things up to speed

--- wp4/views.py
+++ wp4/views.py
@@ -4,8 +4,10 @@
 # from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.utils import timezone
+from .compare.models import Randomisation, PAPER_UNITED_KINGDOM, PAPER_EUROPE
 
 
 # Some forced errors to allow for testing the Error Page Templates
@@ -29,14 +31,41 @@ def dashboard_index(request):
 def wp4_index(request):
     return render(request, 'dashboard/wp4_index.html', {})
 
+
+def _is_coordinator(user):
+    if user.is_superuser:
+        return True
+    return user.groups.filter(name='Central Co-ordinators').exists()
+
 @login_required
+@user_passes_test(_is_coordinator)
 def administrator_index(request):
     return render(request, 'dashboard/administrator_index.html', {})
 
 @login_required
+@user_passes_test(_is_coordinator)
 def administrator_uk_list(request):
-    return render(request, 'dashboard/administrator_index.html', {})
+    randomisation_listing = Randomisation.objects.filter(list_code=PAPER_UNITED_KINGDOM)
+    return render(
+        request,
+        'dashboard/administrator_offline_list.html',
+        {
+            'listing': randomisation_listing,
+            'location': "United Kingdom",
+            'timestamp': timezone.now()
+        }
+    )
 
 @login_required
+@user_passes_test(_is_coordinator)
 def administrator_europe_list(request):
-    return render(request, 'dashboard/administrator_index.html', {})
+    randomisation_listing = Randomisation.objects.filter(list_code=PAPER_EUROPE)
+    return render(
+        request,
+        'dashboard/administrator_offline_list.html',
+        {
+            'listing': randomisation_listing,
+            'location': "Europe",
+            'timestamp': timezone.now()
+        }
+    )
