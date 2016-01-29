86cff719320eba67a5aef63f41c3df579244be62
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2016-01-29 16:21:07 +0000

    Debugging deployment using location.env with testing server

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,6 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Determine location and therefore which settings to consult
+print("DEBUG: getcwd=%s" % os.getcwd())
 with open('location.env', 'r') as location_file:
     environment_string = location_file.read().replace('\n', '')
 
