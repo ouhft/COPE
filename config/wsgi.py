e7393d956b987ea0ac14c636345fce192ac3f388
carl marshall <carl.marshall@nds.ox.ac.uk>
2016-01-29 09:30:58 +0000

    deployment tweak for config

--- config/wsgi.py
+++ config/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Determine location and therefore which settings to consult
-with open('location.env', 'r') as location_file:
+with open('../location.env', 'r') as location_file:
     environment_string = location_file.read().replace('\n', '')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings." + environment_string)
