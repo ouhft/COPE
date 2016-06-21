b05fcf91717b6cf468b211fb691a121b0272cd57
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2016-06-21 17:50:49 +0100

    Backup path changes

--- deploy/production/bin/dbbackup.sh
+++ deploy/production/bin/dbbackup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SOURCE=/sites/cope/cope_repo/db.sqlite3
-TARGET=/sites/cope/db-backup/`date "+%Y-%m-%d"`.sqlite3
+SOURCE=/sites/py3_cope/cope_repo/db.sqlite3
+TARGET=/sites/py3_cope/db-backup/`date "+%Y-%m-%d"`.sqlite3
 
 echo "Starting backup as `whoami`"
 cp $SOURCE $TARGET
