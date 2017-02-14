26590c339d68f9955be91036c544683962a4928e
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2017-02-14 13:05:13 +0000

    Initial migrations to set up the 0.8.0 Schema. Starting to draft the SQL process for migrating the data from the 0.6.4 to 0.8.0 schema

---
+++ deploy/production/DataMigrationTo080.sql
@@ -0,0 +1,5 @@
+/*
+ Script to migrate data safely from a 0.6.4 schema to the new 0.8.0 schema
+ */
+ATTACH DATABASE 'file:///Users/carl/Projects/py3_cope/cope_repo/old.db.sqlite3?mode=ro' AS old;
+
