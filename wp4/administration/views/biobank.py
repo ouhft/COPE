d863994228be8ab699350d1383175c1d8b2d4e24
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2017-06-22 15:36:03 +0100

    Issue #290 - Summary data added to all four sample reports

--- wp4/administration/views/biobank.py
+++ wp4/administration/views/biobank.py
@@ -24,7 +24,46 @@ def blood_collection(request):
 
     listing = BloodSample.objects.all().order_by('collected')
 
-    summary = {}
+    summary = {
+        "total": {"sst": 0, "edsa": 0},
+        "collected": {
+            "unknown": {"sst": 0, "edsa": 0},
+            "no": {"sst": 0, "edsa": 0},
+            "yes": {"sst": 0, "edsa": 0}
+        },
+        "matching": {
+            "collected_matched": {"sst": 0, "edsa": 0},
+            "collected_unmatched": {"sst": 0, "edsa": 0},
+        }
+    }
+
+    for sample in listing:
+        if sample.blood_type == BloodSample.SAMPLE_SST:
+            summary["total"]["sst"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"]["sst"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"]["sst"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"]["sst"] += 1
+                else:
+                    summary["collected"]["no"]["sst"] += 1
+            else:
+                summary["collected"]["unknown"]["sst"] += 1
+        else:
+            summary["total"]["edsa"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"]["edsa"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"]["edsa"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"]["edsa"] += 1
+                else:
+                    summary["collected"]["no"]["edsa"] += 1
+            else:
+                summary["collected"]["unknown"]["edsa"] += 1
 
     return render(
         request,
@@ -44,7 +83,32 @@ def urine_collection(request):
 
     listing = UrineSample.objects.all().order_by('collected')
 
-    summary = {}
+    summary = {
+        "total": 0,
+        "collected": {
+            "unknown": 0,
+            "no": 0,
+            "yes": 0
+        },
+        "matching": {
+            "collected_matched": 0,
+            "collected_unmatched": 0,
+        }
+    }
+
+    for sample in listing:
+            summary["total"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"] += 1
+                else:
+                    summary["collected"]["no"] += 1
+            else:
+                summary["collected"]["unknown"] += 1
 
     return render(
         request,
@@ -65,31 +129,45 @@ def tissue_collection(request):
     listing = TissueSample.objects.all().order_by('collected')
 
     summary = {
-        "total": 0,
+        "total": {"rna": 0, "formalin": 0},
         "collected": {
-            "unknown": 0,
-            "no": 0,
-            "yes": 0
+            "unknown": {"rna": 0, "formalin": 0},
+            "no": {"rna": 0, "formalin": 0},
+            "yes": {"rna": 0, "formalin": 0}
         },
         "matching": {
-            "collected_matched": 0,
-            "collected_unmatched": 0,
+            "collected_matched": {"rna": 0, "formalin": 0},
+            "collected_unmatched": {"rna": 0, "formalin": 0},
         }
     }
 
     for sample in listing:
-        summary["total"] += 1
-        if sample.collected is not None:
-            if sample.collected is True:
-                summary["collected"]["yes"] += 1
-                if sample.wp7_location.count() > 0:
-                    summary["matching"]["collected_matched"] += 1
+        if sample.tissue_type == TissueSample.SAMPLE_R:
+            summary["total"]["rna"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"]["rna"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"]["rna"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"]["rna"] += 1
                 else:
-                    summary["matching"]["collected_unmatched"] += 1
+                    summary["collected"]["no"]["rna"] += 1
             else:
-                summary["collected"]["no"] += 1
+                summary["collected"]["unknown"]["rna"] += 1
         else:
-            summary["collected"]["unknown"] += 1
+            summary["total"]["formalin"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"]["formalin"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"]["formalin"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"]["formalin"] += 1
+                else:
+                    summary["collected"]["no"]["formalin"] += 1
+            else:
+                summary["collected"]["unknown"]["formalin"] += 1
 
     return render(
         request,
@@ -109,7 +187,32 @@ def perfusate_collection(request):
 
     listing = PerfusateSample.objects.all().order_by('collected')
 
-    summary = {}
+    summary = {
+        "total": 0,
+        "collected": {
+            "unknown": 0,
+            "no": 0,
+            "yes": 0
+        },
+        "matching": {
+            "collected_matched": 0,
+            "collected_unmatched": 0,
+        }
+    }
+
+    for sample in listing:
+            summary["total"] += 1
+            if sample.collected is not None:
+                if sample.collected is True:
+                    summary["collected"]["yes"] += 1
+                    if sample.wp7_location.count() > 0:
+                        summary["matching"]["collected_matched"] += 1
+                    else:
+                        summary["matching"]["collected_unmatched"] += 1
+                else:
+                    summary["collected"]["no"] += 1
+            else:
+                summary["collected"]["unknown"] += 1
 
     return render(
         request,
