3285290ba310f612ccce5530c7740593e09ae406
Carl Marshall <carl.marshall@nds.ox.ac.uk>
2017-07-07 16:22:26 +0100

    Debugging deployment issue with test environment - suspect not reading the correct .env file; was wrong... hunting for it, realising I did a workaround for this issue previously

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
+import os
 import environ
 
 from django.core.wsgi import get_wsgi_application
 
-environ.Env.read_env(env_file='../config/settings/.env')
+# Determine location and therefore which settings to consult
+if os.getcwd() == '/home/cm13':
+    environ.Env.read_env(env_file='webapps/wp4_django_py3/cope_repo/config/settings/.env')
+else:
+    environ.Env.read_env(env_file='config/settings/.env')
 
 application = get_wsgi_application()
